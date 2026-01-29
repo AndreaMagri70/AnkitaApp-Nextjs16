77c1a060deb68169de4f86a47d59554331c5fd8c
AndreaMagri70 <assomag@libero.it>
2026-01-29 18:48:00 +0100

    Fix: Inserimento nuovi prodotti

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -1,6 +1,22 @@
+import SectionHeader from "@/components/common/section-header";
+import ProductSubmitForm from "@/components/products/product-submit-form";
+import { SparklesIcon } from "lucide-react";
 
 export default function SubmitPage() {
-  return (
-    <div>SubmitPage</div>
-  )
+    return (
+        <section className="py-20">
+            <div className="wrapper">
+                <SectionHeader
+                    title="Submit Your Project"
+                    icon={SparklesIcon}
+                    description="Share your creation with the community. Your submission wil be reviewed
+        before going live."
+                />
+                <div className="max-w-2xl mx-auto">
+                    <ProductSubmitForm />
+
+                </div>
+            </div>
+        </section>
+    );
 }
