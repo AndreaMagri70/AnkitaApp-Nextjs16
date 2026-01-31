e2dbe0beaca2232cce174a7f05523ddef2fc86a5
AndreaMagri70 <assomag@libero.it>
2026-01-31 11:06:55 +0100

    Fix: Explore Page

--- lib/products/product-select.ts
+++ lib/products/product-select.ts
@@ -1,8 +1,10 @@
 import { db } from "@/db";
 import { products } from "@/db/schema";
 import { desc, eq } from "drizzle-orm";
+import { connection } from "next/server";
 
 export async function getFeaturedProducts() {
+  "use cache";
   const productsData = await db
     .select()
     .from(products)
@@ -12,8 +14,29 @@ export async function getFeaturedProducts() {
   return productsData;
 }
 
+export async function getAllApprovedProducts() {
+  const productsData = await db
+    .select()
+    .from(products)
+    .where(eq(products.status, "approved"))
+    .orderBy(desc(products.voteCount));
+
+  return productsData;
+}
+
+export async function getAllProducts() {
+  "use cache";
+  const productsData = await db
+    .select()
+    .from(products)
+    .orderBy(desc(products.voteCount));
+
+  return productsData;
+}
+
 export async function getRecentlyLaunchedProducts() {
-  const productsData = await getFeaturedProducts();
+  // await connection();
+  const productsData = await getAllApprovedProducts();
   const oneWeekAgo = new Date();
   oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
 
